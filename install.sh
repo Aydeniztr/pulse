2b5d644589ff306f5d765c864a97676892bfc07c
Aydeniztr <ahmet.y.aydeniz@gmail.com>
2026-04-06 03:24:33 +0300

    changed some stuff

--- install.sh
+++ install.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 set -e
 
+REPO_URL="https://github.com/yourusername/cmon.git" # TODO: Update with your real URL
+TMP_DIR=$(mktemp -d)
+
+# Cleanup on exit
+trap "rm -rf $TMP_DIR" EXIT
+
 # Detect Package Manager
 if [ -f /etc/debian_version ]; then
     echo "Detected Debian/Ubuntu-based system. Installing dependencies..."
-    sudo apt update && sudo apt install -y build-essential libncursesw5-dev
+    sudo apt update && sudo apt install -y build-essential libncursesw5-dev git
 elif [ -f /etc/fedora-release ] || [ -f /etc/redhat-release ]; then
     echo "Detected Fedora/RHEL-based system. Installing dependencies..."
-    sudo dnf install -y gcc make ncurses-devel
+    sudo dnf install -y gcc make ncurses-devel git
 elif [ -f /etc/arch-release ]; then
     echo "Detected Arch Linux-based system. Installing dependencies..."
-    sudo pacman -S --noconfirm base-devel ncurses
+    sudo pacman -S --noconfirm base-devel ncurses git
 else
-    echo "Warning: Unsupported OS. Please ensure gcc, make, and ncurses-devel are installed manually."
+    echo "Warning: Unsupported OS. Please ensure gcc, make, git, and ncurses-devel are installed manually."
 fi
 
-# Build
-echo "Building cmon..."
+# Clone and Build in Temp
+echo "Cloning cmon into temporary directory..."
+git clone --depth 1 "$REPO_URL" "$TMP_DIR"
+cd "$TMP_DIR"
+
+echo "Building cmon from source..."
 make
 
 # Install to /usr/local/bin
